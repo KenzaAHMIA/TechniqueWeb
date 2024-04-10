1461bfbf5c7645ca6f2ea8a0b177dd28586cd550
Sazuna <liza.fretel@gmail.com>
2024-04-10 15:45:58 +0200

    Ajout des traductions dynamiquement

--- site/static/api.js
+++ site/static/api.js
@@ -6,7 +6,8 @@ const traduire = $('#traduire');
 const textarea = $('#input-text');
 const api_div = $('#api');
 
-const translatePlus_output = $('#translatePlus-output');
+const apiRes = $("#api-res")
+//const translatePlus_output = $('#translatePlus-output');
 
 // ----------------- language codes ----------------
 // Définition des dictionnaires avec comme clé l'identifiant
@@ -33,35 +34,57 @@ $('form').submit(function(event) {
  text = textarea.val();
  if (text.length == 0) {
   return;
- }else {
-  // TODO afficher la div de chargement
+ }
+  // Affiche la div de chargement avec Spinner à la place du texte
+  // "Traductions trouvées..."
+  $("#spinner").css("display", "block");
+  $("#messageApiOk").css("display","none");
 
-  // ------------ récupération des traductions ----__------
-  getTranslatePlus(textarea.val(), translatePlusLang[lang]);
+  // Cache les balises des anciennes traductions pour les 4 api
+  api_div.removeClass('active');
+  for (let index = 1; index < 4; index++) {
+   $("#api-col-" + index).css("display", "none");
+  }
+
+  // ------------ récupération des traductions ------------
+  // i est le nombre d'API qui ont fonctionné.
+  let i = 1;
+  i = getTranslatePlus(textarea.val(), translatePlusLang[lang], i);
   // TODO appeler les fonctions des autres API
+  // i = getDeepl(textarea.val(), deepLLang[lang], i);
 
   // Affiche la classe des traductions des APIs
+  $("#spinner").css("display", "none");
+  $("#messageApiOk").css("display","block");
   api_div.addClass('active');
- }
+
 });
 
-// ----------------- translateplus -----------------
-// Fonction qui appelle l'API de translatePlus
-// et écrit dans la balise de résultat la traduction
-// obtenue.
-// TODO pour améliorer le résultat, il faut faire en sorte
-// que la balise ne soit pas affichée si l'API ne renvoie
-// rien.
+// ----------------- add_ranslation ----------------
+// Fonction qui ajoute une traduction dans la liste des
+// résultats et l'affiche. Elle doit être appelée uniquement
+// si l'API a donné un résultat.
+function add_translation(text, api_name, i) {
+ $("#api-col-" + i).css("display", "block");
+ $("#api-name-" + i).text(api_name);
+ $("#api-text-" + i).text(text);
+}
+
+// ----------------- translatePlus -----------------
+// Variables de la requête HTTP à envoyer à translatePlus
 const apiUrl_tp = "https://api.translateplus.io/v1/translate";
 const apiKey_tp = '83168afac2c13b8117482eb869bf5872f144ae5c';
 
-function getTranslatePlus(text, target_language) {
-  // Récupération de la langue source si nécessaire
-  /*
-  console.log(text);
+
+// Fonction qui appelle l'API de translatePlus
+// et écrit dans la balise de résultat la traduction
+// obtenue si l'API a réussi. La fonction doit obligatoirement
+// renvoyer une valeur de i. Soit i + 1 si l'API a réussi,
+// soit i si elle n'a pas réussi.
+function getTranslatePlus(text, target_language, i) {
   var data = {
    text: text,
-   source: "auto",
+   source: "auto", // Détection automatique de la langue par translatePlus
    target: target_language
   };
   fetch(apiUrl_tp, {
@@ -80,12 +103,20 @@ function getTranslatePlus(text, target_language) {
     .then(protectedData => {
      var output_text = protectedData.translations.translation;
      // Ajout de la traduction dans la balise
-     translatePlus_output.text(output_text);
+     add_translation(output_text, "translatePlus", i);
+     return i + 1; //Ecris la prochaine api dans la colonne suivante.
     })
     .catch(error => {
      console.error('Error:', error);
-   });*/
-   translatePlus_output.text("Debug mode");
-}
+   });
+   return i;
+
+   // Commenter le code précédent et utiliser ce code en cas de débugage d'une
+   // autre API afin et décommenter les deux lignes suivantes afin
+   // d'économiser des requêtes HTTP vers translatePlus
+
+   //add_translation("Debug mode", "translatePlus", i);
+   //return i + 1; // a fonctionné. 
+ }
 
 });
