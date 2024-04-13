4686233e880660f587720bcb50f78b449a0bd503
Sazuna <liza.fretel@gmail.com>
2024-04-13 18:30:12 +0200

    Ajout du fichier save.js

---
+++ site/static/save.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // star
+    $("#save-edit").click(function() {
+        alert("Saving the changes..."); // supprimer
+	// TODO enregistrer le contenu de edit-textarea dans un cookie
+    });
+});
