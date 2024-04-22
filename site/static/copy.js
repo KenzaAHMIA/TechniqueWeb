e1439f55d55a96e104081e029ae461cf2d50a966
Sazuna <liza.fretel@gmail.com>
2024-04-22 16:08:35 +0200

    bug copy

--- site/static/copy.js
+++ site/static/copy.js
@@ -9,14 +9,17 @@ $(document).ready(function() {
   text = $("#api-text-1").text();
   navigator.clipboard.writeText(text);
  });
+
  $("#copy-output-2").click(function() {
   text = $("#api-text-2").text();
   navigator.clipboard.writeText(text);
  });
+
  $("#copy-output-3").click(function() {
   text = $("#api-text-3").text();
   navigator.clipboard.writeText(text);
  });
+
  $("#copy-output-4").click(function() {
   text = $("#api-text-4").text();
   navigator.clipboard.writeText(text);
@@ -25,4 +28,6 @@ $(document).ready(function() {
  $("#copy-edit").click(function() {
   text = $("#edit-textarea").val();
   navigator.clipboard.writeText(text);
+ });
+
 });
